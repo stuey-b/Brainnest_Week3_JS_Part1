d05b892fd7e41cadb3d10cebd177008ef1018d9b
stuey-b <stuartwbartlett@gmail.com>
2023-03-09 21:55:04 +0900

    remove array and implement RNG.

--- rock_scissors_paper/script.js
+++ rock_scissors_paper/script.js
@@ -4,27 +4,41 @@ let playerScore = 0;
 let currentRound = 1;
 
 const computerPlay = () => {
-  choices = [`rock`, `scissors`, `paper`];
-  const randomChoice = Math.floor(Math.random() * choices.length);
-  return choices[randomChoice];
-}
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return `rock`;
+  } else if (randomNumber === 1) {
+    return `scissors`;
+  } else {
+    return `paper`;
+  }
+};
 
 const playerPlay = () => {
-  const playerSelection = prompt("Make a choice. Type 'rock', 'scissors' or 'paper' below: ");
+  const playerSelection = prompt(`Type 'rock', 'scissors' or 'paper' below: `);
   return playerSelection.toLowerCase();
-}
+};
 
-const nextRound = () => currentRound += 1;
+const nextRound = () => (currentRound += 1);
 
 const displayFinalScore = (playerScore, computerScore) => {
   if (playerScore > computerScore) {
-    return `FINAL RESULT: Your Score: ${playerScore}. Opponents score: ${computerScore}. CONGRATULATIONS! You WON!`
+    return `FINAL RESULT: 
+    Your Score: ${playerScore} 
+    Opponents score: ${computerScore} 
+    CONGRATULATIONS! You WON!`;
   } else if (playerScore < computerScore) {
-    return `FINAL RESULT: Your Score: ${playerScore}. Opponents score: ${computerScore}. OOF! You LOST. Better luck next time.`
+    return `FINAL RESULT: 
+    Your Score: ${playerScore} 
+    Opponents score: ${computerScore}
+    OOF! You LOST. Better luck next time.`;
   } else {
-    return `FINAL RESULT: Your Score: ${playerScore}. Opponents score: ${computerScore}. You TIED!`
+    return `FINAL RESULT: 
+    Your Score: ${playerScore} 
+    Opponents score: ${computerScore}
+    You TIED!`;
   }
-}
+};
 
 const playRound = () => {
   const playerSelection = playerPlay();
@@ -45,7 +59,7 @@ const playRound = () => {
       computerScore += 1;
       return `You LOST! ${computerSelection} beats ${playerSelection}.`;
   }
-}
+};
 
 const game = () => {
   console.log(`Let's play ROCK, SCISSORS, PAPER!`);
@@ -77,10 +91,21 @@ const game = () => {
       nextRound();
     } else {
       console.log(displayFinalScore(playerScore, computerScore));
-      console.log(`Thanks for playing! GAME OVER`);
-      notGameOver = false
+      console.log(`Would you like to play again? Check the prompt box.`);
+      const playAgainInput = prompt(`Would you like to play again? Type 'y' or 'n': `);
+      if (playAgainInput === `y`) {
+        computerScore = 0;
+        playerScore = 0;
+        currentRound = 1;
+        console.clear();
+        game();
+      } else {
+        notGameOver = false;
+        console.clear();
+        console.log(`Thanks for playing! GAME OVER`);
+      }
     }
   }
-}
+};
 
 game();
