717a5bdbf83fe292c7e0eeab742c254a898f5552
stuey-b <stuartwbartlett@gmail.com>
2023-03-12 13:17:24 +0900

    add changes from feedback

--- rock_scissors_paper/script.js
+++ rock_scissors_paper/script.js
@@ -1,16 +1,12 @@
-let notGameOver = true;
-let computerScore = 0;
-let playerScore = 0;
-let currentRound = 1;
-
 const computerPlay = () => {
   const randomNumber = Math.floor(Math.random() * 3);
-  if (randomNumber === 0) {
-    return `rock`;
-  } else if (randomNumber === 1) {
-    return `scissors`;
-  } else {
-    return `paper`;
+  switch (randomNumber) {
+    case 0:
+      return `rock`;
+    case 1:
+      return `scissors`;
+    case 2:
+      return `paper`;
   }
 };
 
@@ -18,8 +14,6 @@ const playerPlay = () => {
   const playerSelection = prompt(`Type 'rock', 'scissors' or 'paper' below: `);
   return playerSelection.toLowerCase();
 };
-
-const nextRound = () => (currentRound += 1);
 
 const displayFinalScore = (playerScore, computerScore) => {
   if (playerScore > computerScore) {
@@ -43,25 +37,29 @@ const displayFinalScore = (playerScore, computerScore) => {
 const playRound = () => {
   const playerSelection = playerPlay();
   const computerSelection = computerPlay();
-  switch (playerSelection + " " + computerSelection) {
-    case `rock rock`:
-    case `scissors scissors`:
-    case `paper paper`:
-      return `You TIED! ${playerSelection} & ${computerSelection}.`;
-    case `paper rock`:
-    case `rock scissors`:
-    case `scissors paper`:
-      playerScore += 1;
-      return `You WON! ${playerSelection} beats ${computerSelection}.`;
-    case `rock paper`:
-    case `scissors rock`:
-    case `paper scissors`:
-      computerScore += 1;
-      return `You LOST! ${computerSelection} beats ${playerSelection}.`;
+  if (playerSelection === computerSelection) {
+    return `You TIED! ${playerSelection} & ${computerSelection}.`;
+  } else {
+    switch (playerSelection + " " + computerSelection) {
+      case `paper rock`:
+      case `rock scissors`:
+      case `scissors paper`:
+        playerScore += 1;
+        return `You WON! ${playerSelection} beats ${computerSelection}.`;
+      case `rock paper`:
+      case `scissors rock`:
+      case `paper scissors`:
+        computerScore += 1;
+        return `You LOST! ${computerSelection} beats ${playerSelection}.`;
+    }
   }
 };
 
 const game = () => {
+  let notGameOver = true;
+  let computerScore = 0;
+  let playerScore = 0;
+  let currentRound = 1;
   console.log(`Let's play ROCK, SCISSORS, PAPER!`);
   console.log(`    
       _______
@@ -88,11 +86,13 @@ const game = () => {
     if (currentRound <= 5) {
       console.log(`********** ROUND ${currentRound} **********`);
       console.log(playRound());
-      nextRound();
+      currentRound++;
     } else {
       console.log(displayFinalScore(playerScore, computerScore));
       console.log(`Would you like to play again? Check the prompt box.`);
-      const playAgainInput = prompt(`Would you like to play again? Type 'y' or 'n': `);
+      const playAgainInput = prompt(
+        `Would you like to play again? Type 'y' or 'n': `
+      );
       if (playAgainInput === `y`) {
         computerScore = 0;
         playerScore = 0;
@@ -101,7 +101,6 @@ const game = () => {
         game();
       } else {
         notGameOver = false;
-        console.clear();
         console.log(`Thanks for playing! GAME OVER`);
       }
     }
